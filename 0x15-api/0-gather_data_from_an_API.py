eddddc145cbc37813a34703b31ab9d6ea0a01235
unknown <firaoltulu5@gmail.com>
2024-01-02 22:19:02 +0300

    my second commit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-"""This Returns to-do list information for a given employee ID."""
+#Author: FiaolTulu
+"""This will returns a to-do list information for a given employee ID."""
 import requests
 import sys
 
 if __name__ == "__main__":
-    locurl = "https://jsonplaceholder.typicode.com/"
-    locuser = requests.get(locurl + "users/{}".format(sys.argv[1])).json()
-    loctodos = requests.get(locurl + "todos", params={"userId": sys.argv[1]}).json()
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(sys.argv[1])).json()
+    todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
 
-    completed = [t.get("title") for t in loctodos if t.get("completed") is True]
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            locuser.get("name"), len(completed), len(loctodos)
-        )
-    )
+    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    print("Employee {} is done with tasks({}/{}):".format(
+        user.get("name"), len(completed), len(todos)))
     [print("\t {}".format(c)) for c in completed]
