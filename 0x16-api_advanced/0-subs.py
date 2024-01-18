cbc75462422133e3b4c287151aacd75a4c692a8e
firaoltulu <firaoltulu5@gmail.com>
2024-01-17 22:51:15 -0800

    my second commit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,10 +8,10 @@ def number_of_subscribers(subreddit):
     import requests
 
     sub_info = requests.get(
-        "https://www.reddit.com/r/{}/about.json".format(subreddit),
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
-    )
+            "https://www.reddit.com/r/{}/about.json".format(subreddit),
+            headers={"User-Agent": "My-User-Agent"},
+            allow_redirects=False,
+            )
     if sub_info.status_code >= 300:
         return 0
 
--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -9,19 +9,19 @@ def count_words(subreddit, word_list, word_count={}, after=None):
     import requests
 
     sub_info = requests.get(
-        "https://www.reddit.com/r/{}/hot.json".format(subreddit),
-        params={"after": after},
-        headers={"User-Agent": "My-User-Agent"},
-        allow_redirects=False,
-    )
+            "https://www.reddit.com/r/{}/hot.json".format(subreddit),
+            params={"after": after},
+            headers={"User-Agent": "My-User-Agent"},
+            allow_redirects=False,
+            )
     if sub_info.status_code != 200:
         return None
 
     info = sub_info.json()
 
     hot_l = [
-        child.get("data").get("title") for child in info.get("data").get("children")
-    ]
+            child.get("data").get("title") for child in info.get("data").get("children")
+            ]
     if not hot_l:
         return None
 
@@ -43,5 +43,5 @@ def count_words(subreddit, word_list, word_count={}, after=None):
         [print("{}: {}".format(k, v)) for k, v in sorted_counts if v != 0]
     else:
         return count_words(
-            subreddit, word_list, word_count, info.get("data").get("after")
-        )
+                subreddit, word_list, word_count, info.get("data").get("after")
+                )
